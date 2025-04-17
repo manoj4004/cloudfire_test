ff54b9f50b723cc6d3f1ee8bd8084e725d3cb38d
manoj4004 <manojkumar.a4004@gmail.com>
2025-04-17 10:03:20 +0530

    Update app.java

--- app.java
+++ app.java
@@ -1,2 +1,6 @@
 Hello All
 I am manoj kumar from Hyderabad
+
+git checkout master: Ravi switches to the master branch, the main version of the project everyone starts with.
+git commit -a -m "My new logo": Ravi saves his change (adding a logo) to his computer with a note ("My new logo"). 
+The -a automatically includes all changed files, and -m adds the message.
