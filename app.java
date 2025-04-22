2e3fb0eaf52227b43a0f64a3f4df2e9e2125ce02
firstname lastname <manojkumar.a4004@gmail.com>
2025-04-22 19:06:49 +0530

    fixing code error as per request

--- app.java
+++ app.java
@@ -1,3 +1,8 @@
 Hello All Welcome to RR Technosoft
-  I am training here or AWS DevOps in RR Technosoft
+
 As per adi request we are changing the code as per requirement
+
+Maven downloads and manages project dependencies from repositories 
+like Maven Central or private repositories.
+Dependencies are declared in the <dependencies> 
+section of the POM file with coordinates (groupId, artifactId, version
